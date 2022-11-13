f16e47910bc1db37809497dc0338291f88d9b3fb
nicholsa1954 <67351799+nicholsa1954@users.noreply.github.com>
2022-11-13 05:28:31 -0600

    added logging

--- flask_app/__init__.py
+++ flask_app/__init__.py
@@ -1,10 +1,13 @@
 """Initialize app."""
+import os
 from flask import Flask
 from flask import flash, redirect,  url_for
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_moment import Moment
+import logging
+from logging.handlers import RotatingFileHandler
 from werkzeug.debug import DebuggedApplication
 
 db = SQLAlchemy()
@@ -61,5 +64,18 @@ def create_app():
             
         if app.debug:
             app.wsgi_app = DebuggedApplication(app.wsgi_app, evalex=True)
-
+            
+        if not app.debug and not app.testing:
+            if not os.path.exists('logs'):
+                    os.mkdir('logs')
+            file_handler = RotatingFileHandler('logs/flask_app.log', maxBytes=10240,
+                                               backupCount=10)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+        
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('Flask_app startup')           
+            
         return app
